d8437c0295e150e76b51dae7eee32a12379b6960
parinitha2111 <42990274+parinitha2111@users.noreply.github.com>
2018-09-26 10:39:51 +0530

    Add files via upload

---
+++ usingfunction.py
@@ -0,0 +1,25 @@
+#using function
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+
+def add():
+	c=a+b
+	return c
+print(("addition of a+b"),add())
+
+def sub():
+	return a-b
+print(("subtraction of a-b"),sub())
+
+def multiplication():
+	return a*b
+print(("multiplication of a*b"),multiplication())
+
+def divide():
+	return a/b
+print(("division of a/b"),divide())	
+
+
+
+
+
