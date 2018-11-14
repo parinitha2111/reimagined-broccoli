7140efa21d7de90002f45bea65420424d2ce7e79
parinitha2111 <42990274+parinitha2111@users.noreply.github.com>
2018-11-14 09:33:17 +0530

    Add files via upload

---
+++ distance.py
@@ -0,0 +1,71 @@
+#Libraries
+import RPi.GPIO as GPIO
+import time
+ 
+#GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+ 
+#set GPIO Pins
+GPIO_TRIGGER = 18
+GPIO_ECHO = 24
+ 
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+ 
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance
+ 
+if __name__ == '__main__':
+    try:
+        while True:
+            dist = distance()
+            print ("Measured Distance = %.1f cm" % dist)
+            time.sleep(1)
+            if (dist>100):
+                print("the distance is above 100.bye!")
+                break
+ 
+        # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
+        
+        
+        
+        
+        
+OUTPUT:
+Measured Distance = 4.4 cm
+Measured Distance = 5.0 cm
+Measured Distance = 4.8 cm
+Measured Distance = 5.8 cm
+Measured Distance = 6.9 cm
+Measured Distance = 10.7 cm
+Measured Distance = 4.0 cm
+Measured Distance = 3325.9 cm
+the distance is above 100.bye!
